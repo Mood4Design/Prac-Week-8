dac2ab9b2cd499e25e2c8a87aec509c0da816590
allan69 <leallan69@gmail.com>
2022-10-04 23:46:01 +1100

    Add Score

--- Prac-Week-8/Assets/Scripts/UIManager.cs
+++ Prac-Week-8/Assets/Scripts/UIManager.cs
@@ -7,21 +7,29 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField]
     private Text scoreText;
+    [SerializeField] private Text scoreText2;
     
     [SerializeField]
     private string scoreFormat = "Score: {0}";
+    [SerializeField]
+    private string scoreFormat2 = "Player2 Score: {0}";
+
 
     private Scorekeeper scorekeeper;
+    
 
     void Start()
     {
         scorekeeper = FindObjectOfType<Scorekeeper>();
+       
     }
 
 
     void Update()
     {
         int score = scorekeeper.Score;
+        int score2 = scorekeeper.Score2;
         scoreText.text = string.Format(scoreFormat, score);
+        scoreText2.text = string.Format(scoreFormat2, score2);
     }
 }
